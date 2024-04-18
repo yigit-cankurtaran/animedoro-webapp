067e23b7bded35c4ae649f8cefa6370c74dc1c51
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-18 18:16:39 +0300

    separated synopsis, cleaned synopsis, started search bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-  const [synopsis, setSynopsis] = useState("");
-
-  useEffect(() => {
-    fetch("https://api.jikan.moe/v4/anime?q=Fate/Zero")
-      // gets data from the Jikan API
-      .then((response) => response.json())
-      // the above line converts the response to JSON
-      // .then((data) => setData(data));
-      .then((data) => {
-        setData(data);
-        setSynopsis(data.data[0].synopsis);
-      });
-    // the above line sets the data to the state
-  }, []);
-
   return (
     <>
       <h1>hello world!</h1>
-      {data && <div>{JSON.stringify(synopsis)}</div>}
+      <Link href="/synopsis">Synopsis</Link>
+      <Link href="/search">Search</Link>
     </>
   );
 }
+// TODO: make search bar
---
+++ src/pages/synopsis/index.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Synopsis() {
+  const [data, setData] = useState([]);
+  const [synopsis, setSynopsis] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.jikan.moe/v4/anime?q=Fate/Zero")
+      // gets data from the Jikan API
+      .then((response) => response.json())
+      // the above line converts the response to JSON
+      // .then((data) => setData(data));
+      .then((data) => {
+        setData(data);
+        setSynopsis(data.data[0].synopsis);
+        // this works, we need to get the first element of the array
+        // it returns an array of objects, we need to get the first object
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    // the above line sets the data to the state
+  }, []);
+
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>
+        {isLoading
+          ? "Loading..."
+          : data && <div>{JSON.parse(JSON.stringify(synopsis))}</div>}
+      </h1>
+    </>
+  );
+  //   parse cleans up the JSON data
+}
