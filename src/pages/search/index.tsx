bd9e4a1bae30d968daa72710b47616bdb30bc89c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-22 22:54:47 +0300

    created the search bar, haven't done the API stuff yet

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,27 @@
+"use client";
+import { useState } from "react";
+
 export default function Search() {
-  return <h1>Search</h1>;
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // prevents the page from reloading
+    const formData = new FormData(event.target as HTMLFormElement);
+    // we need this for the type to be correct
+    const animeName = formData.get("animeName");
+    // alert(animeName);
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="searchInput">
+          Search anime:
+          <input type="text" name="animeName" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
   //   this will search for anime and display them
   //  we will use the Jikan API
   //   display a list of them
