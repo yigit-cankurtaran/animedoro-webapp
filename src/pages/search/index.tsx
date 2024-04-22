160d36d7952f685d5e6cfb6e4ddae9ae66644124
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-22 23:04:19 +0300

    found a way to do it, will check for better

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,13 +2,26 @@
 import { useState } from "react";
 
 export default function Search() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     // prevents the page from reloading
     const formData = new FormData(event.target as HTMLFormElement);
     // we need this for the type to be correct
     const animeName = formData.get("animeName");
-    // alert(animeName);
+
+    fetch(`https://api.jikan.moe/v4/anime?q=${animeName}`)
+      // this might not be the best way to do this
+      // i remember that the Jikan API has a search endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
 
   return (
@@ -19,6 +32,7 @@ export default function Search() {
           <input type="text" name="animeName" />
         </label>
         <input type="submit" value="Submit" />
+        {isLoading ? <p>Loading...</p> : <p>{JSON.stringify(data)}</p>}
       </form>
     </>
   );
