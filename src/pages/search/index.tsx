3da70404a22ead054492e18a2443ace547d0c8ce
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-22 23:30:58 +0300

    error handling through the search component

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,8 @@
-"use client";
 import { useState } from "react";
 
 export default function Search() {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     // prevents the page from reloading
@@ -14,10 +13,18 @@ export default function Search() {
     fetch(`https://api.jikan.moe/v4/anime?q=${animeName}`)
       // this might not be the best way to do this
       // i remember that the Jikan API has a search endpoint
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) return response.json();
+        else throw new Error("Failed to fetch");
+      })
       .then((data) => {
         console.log(data);
         setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setData({});
+        setIsLoading(false);
       })
       .finally(() => {
         setIsLoading(false);
@@ -32,7 +39,14 @@ export default function Search() {
           <input type="text" name="animeName" />
         </label>
         <input type="submit" value="Submit" />
-        {isLoading ? <p>Loading...</p> : <p>{JSON.stringify(data)}</p>}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          data && <p>{JSON.stringify(data.data[0])}</p>
+          // this still returns an array, we can't parse this
+          // i can create a new div and map through the array
+          // displaying some of the data like title, image, etc.
+        )}
       </form>
     </>
   );
