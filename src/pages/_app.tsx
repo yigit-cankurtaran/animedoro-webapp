085aa1db5f1a890a8e665b34ef60b7804b1dd60b
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-24 23:34:23 +0300

    type error fix in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./globals.css"; // Import your global styles here
 import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 
-function MyApp({ Component, pageProps }) {
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: React.ElementType;
+  pageProps: any;
+}) {
   // these show errors but we can fix later
   // it works anyway
   return <Component {...pageProps} />;
