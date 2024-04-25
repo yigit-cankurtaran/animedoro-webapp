0f243ee24b176277ce32c9ab230494f831cf1973
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-25 20:00:33 +0300

    fixing the episodes page

--- src/pages/episodes/[animeId].tsx
+++ src/pages/episodes/[animeId].tsx
@@ -1,7 +1,7 @@
 // name in brackets because it's a dynamic route
 // any anime id will be passed to this page
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function AnimeId() {
   const router = useRouter();
@@ -11,32 +11,44 @@ export default function AnimeId() {
   // there's a filler boolean tag so we can add a switch for it
   const { animeId } = router.query;
   // fetch episodes from the API
-  fetch(`https://api.jikan.moe/v4/anime/${animeId}/episodes`)
-    .then((response) => {
-      if (response.ok) return response.json();
-      else throw new Error("Failed to fetch");
-    })
-    .then((data) => {
-      console.log(data);
-      setData(data);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
-    .catch((error) => {
-      console.error("Error fetching data: ", error);
-    });
+  useEffect(() => {
+    fetch(`https://api.jikan.moe/v4/anime/${animeId}/episodes`)
+      .then((response) => {
+        if (response.ok) return response.json();
+        else throw new Error("Failed to fetch");
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [animeId]);
 
   return (
     <div>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <h1>animeId: {animeId}</h1>
-          {/* successfully gets the ID, next up we'll display the episodes and style it a bit */}
-          <p>{JSON.stringify(data)}</p>
-        </div>
+        data?.data?.length > 0 && (
+          <div>
+            {data.data.map((episode: any) => (
+              <div key={episode.mal_id}>
+                <p>{episode.title}</p>
+                <p>{episode.aired}</p>
+                <p>{episode.filler ? "Filler" : "Not Filler"}</p>
+                <p>{episode.recap ? "Recap" : "Not Recap"}</p>
+                {/* we can use this to display stuff for longer anime */}
+                <p>{episode.synopsis}</p>
+                {/* there is no synopsis or an episode specific picture. hmmm */}
+              </div>
+            ))}
+          </div>
+        )
       )}
     </div>
   );
