7404d23cd35b71b0d6a64149feff666ab878df2b
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-04-25 20:18:08 +0300

    some styling on the episodes page, looks really bad

--- src/pages/episodes/[animeId].tsx
+++ src/pages/episodes/[animeId].tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 export default function AnimeId() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
-  const [episodes, setEpisodes] = useState([]);
+  // const [episodes, setEpisodes] = useState([]);
   const [data, setData] = useState({} as any);
   // there's a filler boolean tag so we can add a switch for it
   const { animeId } = router.query;
@@ -31,27 +31,27 @@ export default function AnimeId() {
     }
   }, [animeId]);
 
-  return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        data?.data?.length > 0 && (
-          <div>
-            {data.data.map((episode: any) => (
-              <div key={episode.mal_id}>
-                <p>{episode.title}</p>
-                <p>{episode.aired}</p>
-                <p>{episode.filler ? "Filler" : "Not Filler"}</p>
-                <p>{episode.recap ? "Recap" : "Not Recap"}</p>
-                {/* we can use this to display stuff for longer anime */}
-                <p>{episode.synopsis}</p>
-                {/* there is no synopsis or an episode specific picture. hmmm */}
-              </div>
-            ))}
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    data?.data?.length > 0 && (
+      <div className="bg-slate-500 text-white min-h-full w-full h-full">
+        {data.data.map((episode: any) => (
+          <div
+            key={episode.mal_id}
+            className="flex text-balance text-center flex-col justify-center items-center m-10 bg-slate-800 p-5 rounded-3xl"
+          >
+            {/* might need episode order too */}
+            <p>{episode.title}</p>
+            <p>{episode.aired}</p>
+            <p>{episode.filler ? "Filler" : "Not Filler"}</p>
+            <p>{episode.recap ? "Recap" : "Not Recap"}</p>
+            {/* we can use this to display stuff for longer anime */}
+            <p>{episode.synopsis}</p>
+            {/* there is no synopsis or an episode specific picture. hmmm */}
           </div>
-        )
-      )}
-    </div>
+        ))}
+      </div>
+    )
   );
 }
