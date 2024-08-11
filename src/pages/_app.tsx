cd129c4172ec479b0324abe935e6d8bba49c5519
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-08-11 22:26:15 +0300

    deleted the query client, maybe i'll re-implement it later on

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,6 @@ import React from "react";
 import "./globals.css"; // Import your global styles here
 import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 import RootLayout from "./layout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 1000 * 60 * 5,
-      // 5 minutes
-      staleTime: 1000 * 60 * 1,
-      // 1 minute
-      // this is the time it will wait before it refetches the data
-    },
-  },
-});
 
 function MyApp({
   Component,
@@ -24,11 +11,9 @@ function MyApp({
   pageProps: any;
 }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </QueryClientProvider>
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
   );
 }
 
