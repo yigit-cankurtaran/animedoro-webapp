3f1cc1e1d0bc16259fffe662c1ecfd30174fe270
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-09-14 22:26:53 +0300

    extra config for toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 import RootLayout from "./layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster, ToastBar } from "react-hot-toast";
 
 // declaring this outside because no backend yet
 // in event that i implement this with a backend
@@ -21,7 +21,31 @@ function MyApp({
 }) {
   return (
     <QueryClientProvider client={myClient}>
-      <Toaster />
+      <Toaster>
+        {(t) => (
+          <ToastBar
+            // configuration for every toast
+            toast={t}
+            style={{
+              background: "#1f2937",
+              color: "#f9fafb",
+              border: "1px solid #1f2937",
+              borderRadius: "4px",
+            }}
+          >
+            {({ icon, message }) => (
+              <>
+                {/* layout for the toast */}
+                {icon}
+                {message}
+                <button onClick={() => toast.dismiss(t.id)}>❌</button>
+                {/* button to close the toast */}
+              </>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
--- src/things/Toast.ts
+++ src/things/Toast.ts
@@ -1,25 +1,11 @@
 import toast from "react-hot-toast";
 
-// export function showToast(message: string, type: "success" | "error") {
-//   toast(message, {
-//     position: "top-right",
-//     duration: 3000,
-//     style: {
-//       background: type === "success" ? "#00FF00" : "#FF0000",
-//     },
-//   });
-// }
-
 const successToast = (message: string) => {
   toast.success(message, {
     position: "top-right",
     duration: 3000,
-    style: {
-      background: "#1f2937",
-      color: "#f9fafb",
-      border: "1px solid #1f2937",
-      borderRadius: "4px",
-    },
+    // we need duration or else it won't show
+    icon: "🎉",
   });
 };
 
@@ -27,12 +13,7 @@ const errorToast = (message: string) => {
   toast.error(message, {
     position: "top-right",
     duration: 3000,
-    style: {
-      background: "#1f2937",
-      color: "#f9fafb",
-      border: "1px solid #1f2937",
-      borderRadius: "4px",
-    },
+    icon: "💥",
   });
 };
 
